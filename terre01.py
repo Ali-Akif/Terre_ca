469557156601134e4e806792daba31a751f47529
Ali-Akif <ali-akif@live.fr>
2023-12-17 18:12:05 +0100

    Première épreuve

---
+++ terre01.py
@@ -0,0 +1,10 @@
+#Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne.
+
+# -*- coding: utf-8 -*-
+
+for l in "abcdefghijklmnopqrstuvwxyz":
+    print(l, end='') # 'chaine vide', on peut faire '\n' et probablement d'autres trucs
+
+print()
+
+input("Entrée pour sortir...")
