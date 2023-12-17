7da2034c5cab767728d9de1faae35bc5c994bf4d
Ali-Akif <ali-akif@live.fr>
2023-12-17 22:37:12 +0100

    Update terre04.py

--- terre04.py
+++ terre04.py
@@ -4,8 +4,17 @@ import sys
 
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-argt_terminal = sys.argv[1]
+if len(sys.argv) > 1 and len(sys.argv) < 3:
 
-index = alphabet.index(argt_terminal)
+    for arg in range(1, len(sys.argv)):
+        argt_terminal = sys.argv[1]
 
-print(alphabet[index:])
+    if argt_terminal in alphabet:
+        index = alphabet.index(argt_terminal)
+        print("".join(alphabet[index :]))
+
+    else:
+        print("L'argument n'est pas dans l'alphabet")
+
+else:
+    print("y'a erreur la frérot")
