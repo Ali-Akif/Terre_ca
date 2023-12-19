d6424c3b12dcef774e12b5cf4ef48c9a47d358d7
Ali-Akif <ali-akif@live.fr>
2023-12-19 15:24:47 +0100

    Update terre04.py

--- terre04.py
+++ terre04.py
@@ -1,18 +1,21 @@
-#Créez un programme qui affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules suivi d’un retour à la ligne.
+#Créez un programme qui permet de déterminer si l’argument donné est un entier pair ou impair..
 
 import sys
 
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if len(sys.argv) <= 2:
 
-
-    
-if len(sys.argv) == 2:
     for i in sys.argv[1:]:
-        if i in alphabet:
-            i = i.lower()
-            index = alphabet.index(i)
-            print("".join(alphabet[index:]))
-        else:
-            print("Pas dans l'alphabet")
+        try:
+            nb = int(i) 
+            if nb % 2 == 0:
+                print("pair")
+            elif nb % 2 != 0:
+                print("impair")
+
+        except ValueError:
+            print("tu ne me la mettras pas à l'envers...")
+            sys.exit(1)
+
+
 else:
-    print("Rentre une lettre de l'alphabet dans le terminal")
+    print("Tu ne me la mettras pas à l'envers...")
