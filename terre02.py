eb3809bc2a756d06afe1eb376bdc2b580cec716c
Ali-Akif <ali-akif@live.fr>
2023-12-22 23:23:52 +0100

    Update terre02.py

--- terre02.py
+++ terre02.py
@@ -1,7 +1,4 @@
-#Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
-
 import sys
 
-arg = sys.argv[1:]
-for i in arg:
+for i in sys.argv[1:]:
     print(i)
