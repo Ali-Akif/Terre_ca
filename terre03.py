fc61c0444dee2bca6d1c6722921c7cf86e5c3660
Ali-Akif <ali-akif@live.fr>
2023-12-22 23:37:11 +0100

    Update terre03.py

--- terre03.py
+++ terre03.py
@@ -1,13 +1,11 @@
 import sys
 
+arg = sys.argv[1].lower()
 
-arg = sys.argv[1]
-print(arg)
+if len(arg) > 1:
+    print("Rentre qu'une seule lettre.")
+    exit()
 
-# if len(arg) > 1:
-#     print("error")
-# a = 97 #variable définie à 97, qui correspond a la position de a dans ascii
-# z = 123 #variable définie à 123, qui correspond a la position de z dans ascii
+for a in range(ord(arg), ord("z")):
+    print(chr(a), end="")
 
-# for alphabet in range(ord(arg), z): 
-#     print(chr(alphabet), end="")
