757e8e86610c41ad427c1f0e9e46ad53a45a76a6
Ali-Akif <ali-akif@live.fr>
2023-12-25 22:58:45 +0100

    Update terre06.py

--- terre06.py
+++ terre06.py
@@ -19,23 +19,4 @@ print()
 
 
     # méthode 3:
-chaine_reverse = ""
-for i in range(len(chaine)-1, -1, -1):
-    chaine_reverse += chaine[i]
-print(chaine_reverse)
-
-
-    # méthode 4:
-chaine_reverse = ""
-for i in range(1, len(chaine) +1):
-    chaine_reverse += chaine[-i]
-print(chaine_reverse)
-
-    # méthode 5:
-chaine_reverse =""
-for i in chaine:
-    chaine_reverse = i + chaine_reverse
-print(chaine_reverse)
-
-    # méthode 5:
 print(chaine[len(chaine):-1:-1])
